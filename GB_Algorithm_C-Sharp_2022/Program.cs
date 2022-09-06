349c045df53588b0dcab091a310a5d8173c712d3
Дмитрий Богачев <dima1983bogachov@gmail.com>
2022-09-06 17:07:17 +0300

    Функция вычисления числа Фибоначчи: через рекурсию и через цикл

--- GB_Algorithm_C-Sharp_2022/Program.cs
+++ GB_Algorithm_C-Sharp_2022/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static int Fib(int x)
+        {
+            if (x == 0) return 0;
+
+            int prev = 0;
+            int next = 1;
+            for (int i = 1; i < x; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ведите номер в последовательности Фибоначчи : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int Recursion = Fibonacci(N+2);
+            int Circle = Fib(N+2);
+
+            Console.WriteLine($"{N}-ое число Фибоначчи = {Recursion} [Рекурсия]");
+            Console.WriteLine($"{N}-ое число Фибоначчи = {Circle} [цикл]");
+            
+        }
+
+    }
+
+}
