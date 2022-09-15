1b5280915652b9ca062e04bd9d5fb8fa1a2c2450
Дмитрий Богачев <dima1983bogachov@gmail.com>
2022-09-15 12:18:35 +0300

    Протестируйте поиск строки в HashSet и в массиве

--- GB_Algorithm_C-Sharp_2022/Program.cs
+++ GB_Algorithm_C-Sharp_2022/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+//using BenchmarkDotNet.Running;
+//using BenchmarkDotNet.Attributes;
+
+
+namespace Lesson_04_Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // BenchmarkRunner.Run(typeof(Program).Assembly);
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadKey();
+        }
+    }
+
+    public class BenchmarkSearch
+    {
+        public int N { get; set; } = 10000;
+
+        string[] strArray;
+        HashSet<string> hashSet;
+
+        string symbols = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFJHIJKLMNOPQRSTUVWXYZ";
+        string search;
+
+        public BenchmarkSearch()
+        {
+            strArray = new string[N];
+            hashSet = new HashSet<string>(N);
+
+            for (int i = 0; i < N; i++)
+            {
+                string tmp = RandomString(20, symbols);
+                strArray[i] = tmp;
+                hashSet.Add(tmp);
+            }
+
+            search = strArray[(new Random()).Next(N - 1)];
+        }
+
+        private string RandomString(int lenght, string symbols)
+        {
+            Random random = new Random();
+            String result = "";
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int x = random.Next(symbols.Length);
+                result += symbols[x];
+            }
+
+            return result;
+        }
+
+       // [Benchmark]
+        public bool SearchInArray()
+        {
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (string.Equals(search, strArray[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+       // [Benchmark]
+        public bool SearchInHashSet()
+        {
+            return hashSet.Contains(search);
+        }
+    }
+}
